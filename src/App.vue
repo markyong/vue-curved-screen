77b225b533b1e51c75fcf259731d4430686bd48d
markyong <mayyyyyyyong@gmail.com>
2019-05-11 23:04:20 +0800

    feat: CurvedScreen init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>vue-curved-screen</h1>
+    <curved-screen>
+      <template #screen11>
+        <img style="width: 100%; height: 100%;" src="./assets/test.png">
+      </template>
+      <template #screen12>
+        <img style="width: 100%; height: 100%;" src="./assets/test.png">
+      </template>
+    </curved-screen>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurvedScreen from './CurvedScreen'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CurvedScreen
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+div, h1 {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #111;
+}
+
+h1 {
+  width: 100%;
+  height: 70px;
+  line-height: 100px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+}
+
+#app {
+  width: 1200px;
+  margin: 0 auto;
+}
+
+#app .screen-item {
+  background-color: #0d1134;
 }
 </style>
---
+++ src/CurvedScreen.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="curved-screen" :style="screenStyle">
+    <div
+      class="screen-column"
+      :style="[screenWidthStyle, screenRotateStyle[index]]"
+      v-for="(columnItem, index) in column"
+      :key="columnItem"
+    >
+      <div
+        class="screen-item"
+        :style="screenHeightStyle"
+        v-for="rowItem in row"
+        :key="rowItem"
+      >
+        <slot :name="`screen${rowItem}${columnItem}`"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurvedScreen',
+  props: {
+    width: {
+      type: Number,
+      default: 1200
+    },
+    height: {
+      type: Number,
+      default: 500
+    },
+    row: {
+      type: Number,
+      default: 3
+    },
+    column: {
+      type: Number,
+      default: 5
+    },
+    screenGap: {
+      type: Number,
+      default: 10
+    },
+    rotateDeg: {
+      type: Number,
+      default: 30
+    },
+    perspective: {
+      type: String,
+      default: '1000px'
+    }
+  },
+  data () {
+    return {
+      transformOrigin: ['0 0', '100% 0'],
+      transformConstant: 70,
+      transformSide: 35,
+      transformMid: 26
+    }
+  },
+  computed: {
+    screenStyle () {
+      return {
+        width: `${this.width}px`,
+        height: `${this.height}px`,
+        perspective: this.perspective
+      }
+    },
+    screenWidth () {
+      return (this.width - (this.column - 1) * this.screenGap) / this.column
+    },
+    screenHeight () {
+      return (this.height - (this.row - 1) * this.screenGap) / this.row
+    },
+    screenWidthStyle () {
+      return {
+        width: this.screenWidth + 'px',
+        marginRight: `${this.screenGap}px`
+      }
+    },
+    screenHeightStyle () {
+      return {
+        height: this.screenHeight + 'px',
+        marginBottom: `${this.screenGap}px`
+      }
+    },
+    isOdd () {
+      return this.column % 2 !== 0
+    },
+    translateDistance () {
+      const radian = (this.rotateDeg * Math.PI / 180).toFixed(2)
+
+      return this.screenWidth * radian + 4
+    },
+    screenRotateStyle () {
+      let mid = Math.floor(this.column / 2)
+      let arr = []
+      const style = {
+        transform: `rotateY(0) translateZ(-${this.translateDistance * mid + this.transformMid}px)`
+      }
+
+      mid = this.isOdd ? mid : mid - 1
+
+      for (let i = 0; i < mid; i++) {
+        arr.push({
+          transform: `rotateY(${this.rotateDeg}deg) ` + 
+            `translateZ(-${this.translateDistance * i}px)` + 
+            `translate(${this.transformSide * i + this.transformConstant}px)`,
+          transformOrigin: this.transformOrigin[0]
+        })
+      }
+
+      this.isOdd
+        ? arr.push(style)
+        : arr.push(style, style)
+
+      for (let i = mid + 1; i < this.column; i++) {
+        arr.push({
+          transform: `rotateY(-${this.rotateDeg}deg)` +
+            `translateZ(-${this.translateDistance * (this.column - i - 1)}px)` + 
+            `translate(-${(this.column - i - 1) * this.transformSide + this.transformConstant}px)`,
+            transformOrigin: this.transformOrigin[1]
+        })
+      }
+
+      return arr
+    }
+  }
+}
+</script>
+
+<style scoped>
+.curved-screen {
+  display: flex;
+}
+
+.screen-column:last-child {
+  margin-right: 0 !important;
+}
+.screen-item {
+  cursor: pointer;
+}
+.screen-item:last-child {
+  margin-bottom: 0 !important;
+}
+</style>
